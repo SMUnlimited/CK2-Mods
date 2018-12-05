bb71da497ead3537b3d95b9656ff2115738b3891
SMUnlimited <SMUnlimited@users.noreply.github.com>
2018-12-05 17:55:07 +0000

    3.0.1 updates

--- SM Defines/common/defines/SM_defines.lua
+++ SM Defines/common/defines/SM_defines.lua
@@ -1,7 +1,9 @@
-NDefines.NDiplomacy.PRESTIGE_OPINION_DIV = 500             -- 5000 prestige is the higher end of the spectrum.
-NDefines.NDiplomacy.PIETY_OPINION_DIV = 100                -- 1000 piety is the higher end of the spectrum.
-NDefines.NTitle.BARON_LANDLESS_SON_PRESTIGE = 0.025
-NDefines.NTitle.COUNT_LANDLESS_SON_PRESTIGE = 0.1
-NDefines.NTitle.DUKE_LANDLESS_SON_PRESTIGE = 0.5
-NDefines.NTitle.KING_LANDLESS_SON_PRESTIGE = 1.0
-NDefines.NTitle.EMPEROR_LANDLESS_SON_PRESTIGE = 2.0          -- larger loss of prestige for higher ranks
+--NDefines.NDiplomacy.PRESTIGE_OPINION_DIV = 500             -- 5000 prestige is the higher end of the spectrum.
+--NDefines.NDiplomacy.PIETY_OPINION_DIV = 100                -- 1000 piety is the higher end of the spectrum.
+--NDefines.NTitle.BARON_LANDLESS_SON_PRESTIGE = 0.025
+--NDefines.NTitle.COUNT_LANDLESS_SON_PRESTIGE = 0.1
+--NDefines.NTitle.DUKE_LANDLESS_SON_PRESTIGE = 0.5
+--NDefines.NTitle.KING_LANDLESS_SON_PRESTIGE = 1.0
+--NDefines.NTitle.EMPEROR_LANDLESS_SON_PRESTIGE = 2.0          -- larger loss of prestige for higher ranks
+NDefines.NMilitary.SHATTERED_RETREAT_PREFERRED_PROVINCES = 1				-- Units will try to move at least this many provinces away in shattered retreat
+NDefines.NMilitary.SHATTERED_RETREAT_MAX_PROVINCES = 4			-- Units can not move longer than this many provinces during a shattered retreat
